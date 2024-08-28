4e67a9aeadb81a70d76b8cfbaf2949be32b19712
WanYou <127921756+qiyuanjia0702@users.noreply.github.com>
2024-08-28 23:45:22 +0800

    更新 zd.js

--- Minby/Mim/js/zd.js
+++ Minby/Mim/js/zd.js
@@ -1,4 +1,4 @@
-document.querySelector('.back-to-top').addEventListener('click', function() {
+ document.querySelector('.back-to-top').addEventListener('click', function() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
