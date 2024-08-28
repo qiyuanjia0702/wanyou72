613e97265cffe59445932a399c943e42121ab507
WanYou <127921756+qiyuanjia0702@users.noreply.github.com>
2024-08-28 14:21:13 +0800

    更新 time.js

--- Minby/Min/static/js/time.js
+++ Minby/Min/static/js/time.js
@@ -12,7 +12,7 @@ function time() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2018, 4, 18, 00, 00, 00)
+    var t1 = Date.UTC(2024, 6, 18, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
