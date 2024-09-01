4c165721c815f76ae317f620a2d15fc532faa1dd
WanYou <127921756+qiyuanjia0702@users.noreply.github.com>
2024-09-01 21:49:17 +0800

    Delete Minby/Mim directory

--- Minby/Mim/js/Mouse.js
+++
@@ -1,7 +0,0 @@
-function mouseOver() {
-    document.getElementById("mainimg").src = "img/1.png"; //鼠标悬浮时的旋转图标
-}
-
-function mouseOut() {
-    document.getElementById('mainimg').src = "img/2.png" //默认的旋转图标
-}
