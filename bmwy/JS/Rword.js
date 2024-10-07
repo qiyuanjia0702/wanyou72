0e8152972335c6ecd13abd58554c3542d110d22b
WanYou <127921756+qiyuanjia0702@users.noreply.github.com>
2024-10-07 14:49:20 +0800

    Delete bmwy directory

--- bmwy/JS/Rword.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.imlcd.cn/yy/api.php')
-    .then(response => response.json())
-    .then(data => {
-        const quote = data.content;
-        document.getElementById('random-quote').textContent = quote;
-    })
-    .catch(error => {
-        console.error('Error fetching quote:', error);
-    });
