9442c14d7dac96c96eb92ad6a7e9b0a29d23ce5b
WanYou <127921756+qiyuanjia0702@users.noreply.github.com>
2024-11-23 06:47:36 +0800

    更新 script.js

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const app = Vue.createApp({
         return {
             isDarkMode: false,
             announcements: [{
-                    title: '服务器更新',
-                    text: '我们已经完成了最新的更新，加入了更多有趣的内容！',
-                    img: 'https://via.placeholder.com/500x200.png?text=公告图片1'
+                    title: '最新消息',
+                    text: '乌托邦探险之旅开始了，祝你们有个愉快的寒假！',
+                    img: 'https://ipv4wp.axzzz.top:9503/f/M95Xhj/2024.11.23_05.30_MpxGfrex.png'
                 },
                 {
-                    title: '活动开始',
-                    text: '新的活动已经开始，快来参加吧！',
-                    img: 'https://via.placeholder.com/500x200.png?text=公告图片2'
+                    title: '活动娱乐',
+                    text: '招五名服务器管理员，不低于18岁，会基础的指令和Linux常用命令',
+                    img: 'https://ipv4wp.axzzz.top:9503/f/4wgPTn/2024.11.23_06.38_JWwLvmEM.png'
                 }
             ],
             exhibitionImages: [
@@ -20,15 +20,15 @@ const app = Vue.createApp({
             ],
             servers: [{
                     name: '生存服务器',
-                    description: '这是一个纯净生存服务器，适合喜欢自由建造的玩家。',
-                    ip: '192.168.0.1',
+                    description: '本服务器是1.20版本，确保你的游戏版本正确，该服务器添加更多插件，玩家可以使用tpa指令进行互相传送',
+                    ip: 'su7.candycake.cloud:36472',
                     img: 'https://ipv4wp.axzzz.top:9503/f/e0OahQ/2024.11.23_05.30_xIYhCaEh.png'
                 },
                 {
-                    name: 'PVP服务器',
-                    description: '如果你喜欢战斗，这里是你的天堂！',
-                    ip: '192.168.0.2',
-                    img: 'https://ipv4wp.axzzz.top:9503/f/M95Xhj/2024.11.23_05.30_MpxGfrex.png'
+                    name: '乌托邦探险之旅',
+                    description: '该游戏是由B站limit小火柴制作的大型国创休闲类整合包，更多玩法等着你来探索，所以…赶紧上号！不然刷矿bug就要被修复了！',
+                    ip: 'neo.candycake.cloud:30475',
+                    img: 'https://ipv4wp.axzzz.top:9503/f/Po5MsR/2024.11.23_05.30_fgfZEkgM.png'
                 }
             ],
             lightboxImage: null
@@ -46,7 +46,7 @@ const app = Vue.createApp({
             tempInput.select();
             document.execCommand('copy');
             document.body.removeChild(tempInput);
-            alert('服务器地址已复制：' + ip);
+            alert('服务器地址已复制');
         },
         showImage(image) {
             this.lightboxImage = image;
